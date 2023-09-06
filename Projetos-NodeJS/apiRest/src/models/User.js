23d8c579c95fb48469e3e8122964eb92b9cf4f7c
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-09-06 17:23:23 -0300

    Projeto API REST - Users criados

---
+++ Projetos-NodeJS/apiRest/src/models/User.js
@@ -0,0 +1,47 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'E-mail já registrado',
+        },
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'O nome precisa ter no mínimo 3 caracteres',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 20],
+            msg: 'campo senha precisa ter no mínimo 3 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+    return this;
+  }
+}
