6de45632150a4f0cce3d40f48489a1d9f7465cee
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-09-08 14:58:22 -0300

    REST API - Tratando arquivos

--- Projetos-NodeJS/apiRest/src/controllers/Foto.js
+++ Projetos-NodeJS/apiRest/src/controllers/Foto.js
@@ -1,8 +1,18 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-
-
-    return res.json();
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+      return res.json(req.file);
+    });
   }
 }
 
