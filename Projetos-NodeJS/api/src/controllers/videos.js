3735e24dce3e1d72e97dc8e512a90536d1a7d55f
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-09-12 13:30:33 -0300

    API - Finalizada

--- Projetos-NodeJS/api/src/controllers/videos.js
+++ Projetos-NodeJS/api/src/controllers/videos.js
@@ -1,17 +1,38 @@
-const DatabaseMemory = require('../config/database-memory');
-const database = new DatabaseMemory();
+// const DatabaseMemory = require('../config/database-memory');
+// const database = new DatabaseMemory();
+const { Op } = require('sequelize');
 const Video = require('../model/video');
 
-module.exports.index = (req, res) => {
-    const { search } = req.query;
-    const videos = database.list(search);
+module.exports.index = async (req, res) => {
+    try {
+        const { search } = req.query;
 
-    return res.json(videos);
+        if (search) {
+            const videosDB = await Video.findAll({
+                where: {
+                    title: {
+                        [Op.like]: `%${search}%`,
+                    },
+                },
+            });
+
+            return res.json(videosDB);
+        }
+
+        const videosDB = await Video.findAll();
+
+        return res.json(videosDB);
+
+    } catch (e) {
+        return res.status(400).json({
+            errors: 'Nenhum videos registrados',
+        });
+    }
 };
 
 module.exports.store = async (req, res) => {
     try {
-        database.create(req.body);
+        // database.create(req.body);
         await Video.create(req.body);
 
         return res.status(201).json({ success: 'Criado !' });
@@ -22,16 +43,44 @@ module.exports.store = async (req, res) => {
     }
 };
 
-module.exports.update = (req, res) => {
-    const videoId = req.params.id;
-    database.update(videoId, req.body);
+module.exports.update = async (req, res) => {
+    // const videoId = req.params.id;
+    // database.update(videoId, req.body);
+    try {
+        const { id } = req.params;
+        const video = await Video.findByPk(id);
 
-    return res.status(204);
+        if (!video) return res.status(400).json({
+            errors: 'Video não encontrado',
+        });
+
+        const videoAtt = await video.update(req.body);
+
+        return res.json(videoAtt);
+    } catch (e) {
+        return res.status(400).json({
+            errors: e.errors.map((err) => err.message),
+        });
+    }
 };
 
-module.exports.delete = (req, res) => {
-    const videoId = req.params.id;
-    database.delete(videoId);
+module.exports.delete = async (req, res) => {
+    // const videoId = req.params.id;
+    // database.delete(videoId);
+    try {
+        const { id } = req.params;
+        const video = await Video.findByPk(id);
 
-    return res.status(200);
+        if (!video) return res.status(400).json({
+            errors: 'Video não encontrado',
+        });
+
+        await video.destroy();
+
+        return res.status(200).json({
+            success: 'Video deletado com sucesso !',
+        });
+    } catch (e) {
+        return res.status(400);
+    }
 };
