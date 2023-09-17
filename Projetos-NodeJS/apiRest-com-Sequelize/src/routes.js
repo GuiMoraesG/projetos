0da8eb85fbad96a5eab177d1c57e56274e175b7d
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-09-17 16:57:36 -0300

    API com Sequelize

--- Projetos-NodeJS/apiRest-com-Sequelize/src/routes.js
+++ Projetos-NodeJS/apiRest-com-Sequelize/src/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const UserController = require('./controllers/UserController');
 const AddressController = require('./controllers/AddressController');
 const TechController = require('./controllers/TechController');
+const ReportController = require('./controllers/ReportController');
 
 router.post('/users', UserController.store);
 router.get('/users', UserController.index);
@@ -14,5 +15,7 @@ router.get('/users/:user_id/address', AddressController.index);
 router.post('/users/:user_id/techs', TechController.store);
 router.get('/users/:user_id/techs', TechController.index);
 router.delete('/users/:user_id/techs', TechController.delete);
+
+router.get('/report', ReportController.show);
 
 module.exports = router;
