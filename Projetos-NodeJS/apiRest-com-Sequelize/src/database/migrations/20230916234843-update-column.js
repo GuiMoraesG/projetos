92c0620676fc317297d3f0632de712a01b37ab32
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-09-16 21:00:28 -0300

    API com Sequelize - migration criadas

---
+++ Projetos-NodeJS/apiRest-com-Sequelize/src/database/migrations/20230916234843-update-column.js
@@ -0,0 +1,14 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'users',
+      'email',
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    );
+  },
+
+  down: (queryInterface, Sequelize) => { }
+};
