af2bd1dc1c68310f1df84e2abb47d2eaa5fa7bbb
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-01-07 13:04:00 -0300

    teste

--- Projetos-NodeJS/atividade2/src/database/index.js
+++ Projetos-NodeJS/atividade2/src/database/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const database = require('../config/database');
 const User = require('../models/User');
 
-const modelss = [User];
+const models = [User];
 const connection = new Sequelize(database);
 
-modelss.forEach((model) => model.init(connection));
+models.forEach((model) => model.init(connection));
